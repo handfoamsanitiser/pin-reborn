c6b07f879b146a42ccd6c9191bde6c143b7763c0
Alexey Maleev <alexey.v.maleev@gmail.com>
2026-03-16 15:38:01 +0000

    Draws rectangle

--- src/main.c
+++ src/main.c
@@ -48,12 +48,21 @@ int main(void) {
 	
 	GLfloat vertices[] = {
 		-0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f,
+		0.5f, 0.5f, 0.0f,
 		0.5f, -0.5f, 0.0f
+	};
+	
+	GLuint indices[] = {
+		0, 1, 2,
+		0, 2, 3
 	};
 	
 	GLuint VBO;
 	glGenBuffers(1, &VBO);
+	
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
 	
 	GLuint VAO;
 	glGenVertexArrays(1, &VAO);
@@ -62,6 +71,9 @@ int main(void) {
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);  
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
 	
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);  
@@ -72,7 +84,7 @@ int main(void) {
 		
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 	}
 	
 	free((char*)vertSource);
